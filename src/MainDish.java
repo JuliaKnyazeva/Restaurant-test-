d644985670fe438413bc589353687590704220c5
Julia Knyazeva <jknyazeva@yandex.ru>
2021-04-28 12:03:45 +0300

    created field & constructor in BaseDish, MainDish extends BaseDish

--- src/MainDish.java
+++ src/MainDish.java
@@ -1,26 +1,10 @@
-public class MainDish {
+public class MainDish extends BaseDish{
 
-    private String mainDishName;
-    private double mainDishPrice;
 
-    MainDish(String name, double price) {
-        this.mainDishName = name;
-        this.mainDishPrice = price;
+
+    public MainDish(String name, double price) {
+        super(name, price);
     }
 
-    public String getMainDishName() {
-        return mainDishName;
-    }
 
-    public void setMainDishName(String name) {
-        this.mainDishName = name;
-    }
-
-    public double getMainDishPrice() {
-        return mainDishPrice;
-    }
-
-    public void setMainDishPrice(double price) {
-        this.mainDishPrice = price;
-    }
 }
